a34d660398d80eefc5f70ecc616fdf769f45c746
ManuelExpunged <manuelstraussaigner@gmail.com>
2021-12-06 06:25:12 +0100

    add pic

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # YouTubeCaptions
 
 Small chrome extension which displays YouTube captions for quick reading.
+
+![alt text](https://github.com/ManuelExpunged/YouTubeCaptions/blob/main/assets/capture.png?raw=true)
--- background.js
+++ background.js
@@ -1,16 +1,19 @@
-chrome.action.onClicked.addListener(() => {
-  
+function callback() {
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-  
-    chrome.tabs.sendMessage(tabs[0].id, {text: 'get_dom'}, (dom) => {
-        
+    chrome.tabs.sendMessage(tabs[0].id, {text: 'get_dom'}, (dom) => {       
       const re = new RegExp(/playerCaptionsTracklistRenderer.*?(youtube.com\/api\/timedtext.*?)"/);
       const match = re.exec(dom);
-
+  
       if (match != null) {
         const url = match[1];
         chrome.tabs.create({ url: chrome.runtime.getURL(`youcap.html?url=${encodeURIComponent(url)}`) });
       }
     });
+  });
+}
+
+chrome.action.onClicked.addListener(() => {  
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.tabs.reload(tabs[0].id, null, callback);
   });
 });
